973e2d1da06042be5e3d75a3fd14480de8b7fe82
USER NAME <andrej_mochalov@mail.ru>
2021-11-29 18:01:57 +0300

    random

--- app/src/main/java/com/example/mochalovfirst/MainActivity.kt
+++ app/src/main/java/com/example/mochalovfirst/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mochalovfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var hellowTextView : TextView
+    lateinit var newTextButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hellowTextView = findViewById(R.id.text_view)
+        newTextButton = findViewById(R.id.new_text_button)
+        newtextbuttonClick()
+
+    }
+
+    fun newtextbuttonClick() {
+        newTextButton.setOnClickListener {
+            hellowTextView.text = Random.nextInt().toString()
+        }
     }
 }
