9a5338c2bf5bb0826d3188507feca8b2d7d690b4
USER NAME <andrej_mochalov@mail.ru>
2021-12-01 16:42:02 +0300

    reversescreenrandom

--- app/src/main/java/com/example/mochalovfirst/MainActivity.kt
+++ app/src/main/java/com/example/mochalovfirst/MainActivity.kt
@@ -2,25 +2,41 @@ package com.example.mochalovfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
 
+const val Tag = "MainAktivity"
+const val Key = "randomText"
 class MainActivity : AppCompatActivity() {
-    lateinit var hellowTextView : TextView
-    lateinit var newTextButton : Button
+    lateinit var randomTextView: TextView
+    lateinit var randomButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hellowTextView = findViewById(R.id.text_view)
-        newTextButton = findViewById(R.id.new_text_button)
-        newtextbuttonClick()
-
-    }
-
-    fun newtextbuttonClick() {
-        newTextButton.setOnClickListener {
-            hellowTextView.text = Random.nextInt().toString()
+        randomTextView = findViewById(R.id.random_text_view)
+        randomButton = findViewById(R.id.random_button)
+        if(savedInstanceState != null){
+            randomTextView.text = savedInstanceState.getString(Key)
         }
+
+        randomButton.setOnClickListener {
+            randomize()
+        }
+
     }
+
+    fun randomize() {
+        randomTextView.text = Random.nextInt(until = 100).toString()
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(Key,randomTextView.text.toString())
+    }
+
+
 }
